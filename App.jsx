1fd8038c3e971c44872036338ea95b1eccead045
Nyxn Clint <168699316+ClintNyxn@users.noreply.github.com>
2026-04-20 05:02:29 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,12 +1,14 @@
 import { createRoot } from "react-dom/client";
 import Sidebar from './src/components/sidebar'
 import Icon_name from "./src/components/icon_name";
+import Footer_comp from "./src/components/footer_comp";
 
 function Main(){
     return(
         <div style={{display: 'flex',}}>
             <Sidebar />
-            <Icon_name />
+            <Icon_name /> 
+            <Footer_comp />
         </div>
     )
 }
